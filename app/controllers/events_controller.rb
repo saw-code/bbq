bf249fe45205b48886d16324b0b46cb9632af54f
saw-code <saw.coding@gmail.com>
2021-11-26 23:05:32 +0300

    fix all aplication (add localization)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -56,7 +56,7 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.html { redirect_to events_url, notice: t("controllers.events.destroyed") }
       format.json { head :no_content }
     end
   end
